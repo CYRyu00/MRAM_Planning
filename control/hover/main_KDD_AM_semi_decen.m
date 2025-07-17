9a0cc35994ac954abe778057b6095eaa95cb79f8
unknown <rcy1104@snu.ac.kr>
2025-07-17 16:53:28 +0900

    feat : implement adaptive control

--- control/hover/main_KDD_AM_semi_decen.m
+++ control/hover/main_KDD_AM_semi_decen.m
@@ -1,18 +1,17 @@
 addpath("../dynamics", "../functions", "../../params" )
 clear; close all
-params = define_params();
-m0 = params{1}; I0 = params{2}; mu = params{3}; r = params{4}; d = params{5};
-%m0 = 2.5; I0 = 
+params = define_params_ver2();
+m0 = params{1} / 1.5; I0 = params{2}; mu = params{3}; r = params{4}; d = params{5};
 thrust_limit= params{6}; gravity = params{16};
 dt_sim = 0.001;
-N_sim = 15000;
+N_sim = 20000;
 %% inertia
 shape = [1 1 1; % <- x | y
          0 2 0; %      v
-         0 1 0];
+         1 1 0];
 shape_mass = [m0,  m0, m0;
               m0,  m0 * 1.5, m0;
-              m0, m0, m0];
+              m0,  m0, m0];
 shape_idx = shape;
 [core_row, core_col] = find(shape == 2);
 [AMs_rows, AMs_cols] = find(shape ~= 0);
@@ -44,7 +43,7 @@ wn = 1; damp = 1.2; % 1, 1.2
 kp_M = wn^2; 
 kv_M = 2 * damp *sqrt(kp_M);
 
-wn = 3; damp = 1.2; % 3, 1.2
+wn = 1; damp = 1.2; % 1, 1.2
 kp_z = wn^2; 
 kv_z = 2 * damp *sqrt(kp_z);
 
@@ -53,22 +52,29 @@ kv_M = diag([kv_M, kv_M, kv_z]);
 
 kw_I = 10; %10
 
-epsilon = kv_M(1, 1) * 0.1;
-alpha = 10 / m0; % 10 / m0
-gamma = 2 / m0; % 10 / m0 
+%regulizer
+k_lambda = 0.0 / norm(gravity);
+k_delta = 1 / norm(gravity);
+
+epsilon = kv_M(1, 1) * 0.3;
+alpha = 30 / m0; % 30 / m0
+gamma = 50 / m0; % 50 / m0 
+beta = diag([1 1 5]) * m0 * norm(gravity) * 3; % 1 1 5 * m0 * norm(gravity) * 3
 N_sim_tmp = 10000;
 
-uncetainty = 1.00; % mass, inertia
+mass_uncertainty = 1.10; 
+inertia_uncertainty = 1.10;
+thrust_limit = thrust_limit * 3;
+
 %disturbance
-sigma = 0.05; mean = 0.00; max_val = 0.3;
+sigma = 0.000; mean = 5.0; max_val = 30.0;
 disturb_sim = zeros(N_sim, 6);
-disturb = mean * [0; 0; 0; 0; 0; 1];
-% w_estimation error
+disturb = mean * [0; 0; 0; 0.5; -0.7; -1.0];
+% X, w_estimation error
 X_error = zeros(3, num_AMs);
 w_error = zeros(3, num_AMs);
-sigma_X = 2 / 100; max_X = 0.1; 
-sigma_w = 2 / 100; max_w = 0.1;
-
+sigma_X = 0 / 100; max_X = 0.1; % delicate
+sigma_w = 2 / 100; max_w = 0.1; 
 
 rng('shuffle')
 
@@ -76,9 +82,9 @@ X_hover = [1; 2; 3] * 1e-1; yaw_hover = 0 / 180 *pi;
 [X_des, Xd_des, Xdd_des, yaw_des, yawd_des, yawdd_des] = get_traj_hover(X_hover, yaw_hover, N_sim, dt_sim);
 radius = 0.3;  v_z = 0.05;
 omega     = 2 * pi * 0.1; 
-omega_yaw = 2 * pi * 0.02; 
+omega_yaw = 2 * pi * 0.05; 
 X_hover = [0; 0; 0.5]; yaw_hover = 0 / 180 *pi; 
-[X_des, Xd_des, Xdd_des, yaw_des, yawd_des, yawdd_des] = get_traj_helix(radius, omega, omega_yaw, v_z, X_hover, yaw_hover, N_sim, dt_sim);
+%[X_des, Xd_des, Xdd_des, yaw_des, yawd_des, yawdd_des] = get_traj_helix(radius, omega, omega_yaw, v_z, X_hover, yaw_hover, N_sim, dt_sim);
 %%
 B = [r r -r -r;  -r r r -r; mu -mu mu -mu; 1 1 1 1];
 
@@ -92,13 +98,13 @@ R = eye(3, 3);
 Rd = R * S(w);
 V = [w; R' * Xd];
 
-force_prev = ones(num_AMs) * m0 * 1.0;
-force = ones(num_AMs) * m0 * 1.0;
+force_prev = ones(num_AMs) * m0 * 0.7;
+delta_hat = zeros(3, num_AMs);
 w_des_prev = zeros(3, num_AMs);
 
 X_hist = []; Xd_hist = []; w_hist = []; wd_hist = []; R_hist = cell(N_sim, 1); 
 w_des_hist = []; wd_des_hist = []; thrusts_hist = []; F_hat_hist = [];
-e_p_hist = []; e_d_hist = [];e_w_hist = []; nu_e_hist = [];
+e_p_hist = []; e_d_hist = [];e_w_hist = []; nu_e_hist = []; delta_hat_hist = []; delta_tilde_hist = [];
 times = [];
 
 for i = 1:N_sim_tmp
@@ -123,13 +129,12 @@ for i = 1:N_sim_tmp
     w_error = min(max(w_error, - max_w), max_w);
 
     for j = 1:num_AMs
-        if true %mod(i, 10) == 1
+        
         % position control - backstepping
         mj = mass_ams(j);
         kp_j = mj * kp_M;
         kv_j = mj * kv_M;
         
-        % TODO : estimation error
         e_p  = (1 - X_error(:, j)).*X - X_des(:, i); % could be computed using X_com = X_j - R' * r_cj
         e_pd = (1 - X_error(:, j)).*Xd - Xd_des(:, i);
         e_pdd_hat = gravity + force_prev(j) / mj * R * e_3 - Xdd_des(:, i);
@@ -139,21 +144,31 @@ for i = 1:N_sim_tmp
             Xddd_des = (Xdd_des(:, i+1) - Xdd_des(:, i)) / dt_sim;
         end
         
-        nu_ej = force_prev(j) * R *e_3 - mj * Xdd_des(:, i) + kv_j * e_pd + kp_j * e_p + mj * gravity;
-        eta = - alpha * nu_ej ...
+        %regulizer : not stable
+        if i > 1
+            force_prev(j) = force_prev(j) + k_lambda * (mj / AM_mass * force_tot - force_prev(j));
+        end
+
+        nu_ej = force_prev(j) * R *e_3 /mj - Xdd_des(:, i) + kv_j * e_pd / mj ...
+                + kp_j * e_p / mj + gravity + delta_hat(:, j) / mj;
+        eta = - alpha * mj * nu_ej ...
               - mj * gamma * (e_pd + epsilon * e_p);
-        vec = R' * (eta + mj * Xddd_des + kv_j * e_pdd_hat + kp_j * e_pd);
+        vec = R' * (eta - mj * Xddd_des + kv_j * e_pdd_hat + kp_j * e_pd + delta_hat(:, j));
         w_xj_des = - vec(2) / force_prev(j);
         w_yj_des = vec(1) / force_prev(j);
         forced = vec(3);
 
         force_j = force_prev(j) + forced * dt_sim;
         force_prev(j) = force_j;
-        end
+        
+        % adaptive control
+        delta_hatd = mj / AM_mass * beta * (e_pd + epsilon * e_p + kv_j / mj / gamma * nu_ej); 
+        delta_hat(:, j) = delta_hat(:, j) + delta_hatd * dt_sim;
 
         % rotaion control
         Ij = I_cj{j};
         kw_j = kw_I * norm(Ij);
+        
         
         w_des = [w_xj_des; w_yj_des; yawd_des(i)]; 
         if i > 1
@@ -163,14 +178,18 @@ for i = 1:N_sim_tmp
         end
         w_des_prev(:, j) = w_des;
 
-        wj = (1 - w_error(:, j)).*w; % TODO : estimation error
+        wj = (1 - w_error(:, j)).*w;
         e_w = wj - w_des;
-        %tau_j = S(wj) * Ij * wj + Ij * wd_des - kw_j * e_w - S(r_cj{j}) * [0; 0; force_j];
-        tau_j = S(wj) * Ij * wj + Ij * wd_des - kw_j * e_w; % assumme shape is symetric
+        tau_j = S(wj) * Ij * wj + Ij * wd_des - kw_j * e_w;
         
         lambda_j = inv(B) * [tau_j; force_j];
+        lambda_j = min(thrust_limit, max(-thrust_limit, lambda_j));
         thrusts = [thrusts; lambda_j];
 
+        tau_j = B(1:3,:) * lambda_j;
+        force_j = B(4, :) * lambda_j;
+        
+        
         tau_tot = tau_tot + tau_j + S(r_cj{j}) * [0; 0; force_j];
         force_tot = force_tot + force_j;
     end
@@ -182,9 +201,8 @@ for i = 1:N_sim_tmp
     disturb_sim(i,:) = disturb;
 
     % Dynamics
-    G = G * uncetainty;
-    g = g * uncetainty;
-    C = C * uncetainty;
+    G = G * diag([ones(3,1) * inertia_uncertainty; ones(3,1) * mass_uncertainty]);
+    g = g * mass_uncertainty;
     Vd = inv(G) * ([tau_tot; 0; 0; force_tot] + disturb - g - C * V);
     Rd = R * S(w);
     
@@ -215,6 +233,8 @@ for i = 1:N_sim_tmp
     e_d_hist = [e_d_hist, e_pd]; 
     e_w_hist = [e_w_hist, e_w];
     nu_e_hist = [nu_e_hist, nu_ej];
+    delta_hat_hist = [delta_hat_hist, delta_hat(:, j)];
+    delta_tilde_hist = [delta_tilde_hist, mj / AM_mass * disturb(4:6) - delta_hat(:, j)];
 
     times = [times; i * dt_sim];
 end
@@ -259,7 +279,7 @@ legend({'$\omega_x$', '$\omega_x^{\mathrm{des}}$', ...
         '$\omega_z$', '$\omega_z^{\mathrm{des}}$'}, ...
        'Interpreter', 'latex','FontSize', 12);
 title('${\omega}$ vs Desired', 'Interpreter', 'latex','FontSize', 14)
-ylim([-1, 1])
+%ylim([-1, 1])
 grid on
 
 % 4. wd plot
@@ -274,14 +294,14 @@ legend({'$\dot{\omega}_x$', '$\dot{\omega}_x^{\mathrm{des}}$', ...
         '$\dot{\omega}_z$', '$\dot{\omega}_z^{\mathrm{des}}$'}, ...
         'Interpreter','latex','FontSize', 12);
 title('$\dot{{\omega}}$ vs Desired', 'Interpreter', 'latex','FontSize', 14)
-ylim([-1, 1])
+%ylim([-1, 1])
 grid on
 
 % 7. thrusts
 subplot(3,2,5)
 hold on
 plot(times, thrusts_hist, 'LineWidth', 1.0);
-ylim([-5* thrust_limit, 5* thrust_limit] )
+ylim([ -thrust_limit, thrust_limit] )
 title('Thrusts', 'Interpreter', 'latex','FontSize', 14)
 grid on
 
@@ -290,7 +310,7 @@ figure('Position',[900 50 1000 700]);
 colors = lines(6);
 
 %e_p
-subplot(2,2,1)
+subplot(3,2,1)
 hold on
 for j = 1:3
     plot(times, e_p_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
@@ -300,7 +320,7 @@ title('$e_p$', 'Interpreter', 'latex','FontSize', 14)
 grid on
 
 %e_d
-subplot(2,2,2)
+subplot(3,2,2)
 hold on
 for j = 1:3
     plot(times, e_d_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
@@ -310,23 +330,42 @@ title('$e_v$', 'Interpreter', 'latex','FontSize', 14)
 grid on
 
 %e_w
-subplot(2,2,3)
+subplot(3,2,3)
 hold on
 for j = 1:3
     plot(times, e_w_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
 end
 legend({'$x$','$y$','$z$'},'Interpreter','latex','FontSize', 12);
 title('$e_w$', 'Interpreter', 'latex','FontSize', 14)
-ylim([-1, 1])
+%ylim([-1, 1])
 grid on
 
 %nu_e
-subplot(2,2,4)
+subplot(3,2,4)
 hold on
 for j = 1:3
-    mask = ~isoutlier(nu_e_hist(j, :));
-    plot(times(mask), nu_e_hist(j, mask), 'Color', colors(j,:), 'LineWidth', 1.0);
+    plot(times, nu_e_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
 end
 legend({'$x$','$y$','$z$'},'Interpreter','latex','FontSize', 12);
-title('$\nu_e$', 'Interpreter', 'latex','FontSize', 14)
+title('$\nu_{e,i}$', 'Interpreter', 'latex','FontSize', 14)
+grid on
+
+%delta_hat
+subplot(3,2,5)
+hold on
+for j = 1:3
+    plot(times, delta_hat_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
+end
+legend({'$x$','$y$','$z$'},'Interpreter','latex','FontSize', 12);
+title('$\hat{\Delta}_{p,i}$', 'Interpreter', 'latex','FontSize', 14)
+grid on
+
+%delta_tilde
+subplot(3,2,6)
+hold on
+for j = 1:3
+    plot(times, delta_tilde_hist(j, :), 'Color', colors(j,:), 'LineWidth', 1.0);
+end
+legend({'$x$','$y$','$z$'},'Interpreter','latex','FontSize', 12);
+title('$\tilde{\Delta}_{p,i}$', 'Interpreter', 'latex','FontSize', 14)
 grid on
--- params/define_params_ver2.m
+++ params/define_params_ver2.m
@@ -1,13 +1,13 @@
-function params = define_params()
-I0 = [16.571710 0.830806 0.718277;
-    0.830806 16.655602 1.800197;
-    0.718277 1.800197 29.261652] * 10e-6;
-thrust_limit = 0.15 ;
-m0 = 0.028;
+function params = define_params_ver2()
+I0 = [0.001202, 0.0, 0.0002;
+      0.0, 0.01056, 0.0;
+      0.0002, 0.0, 0.0195];
+thrust_limit = 20.0;
+m0 = 1.8;
 
-mu = 0.005964552;
-r = 0.092*sqrt(2)/4; % 0.032
-d = 0.1; % com to com
+mu = 0.02;
+r = 0.25; 
+d = 0.4; % com to com
 kt = 0.1; % Nm/rad at the handle
 
 c_1 = 0.3; % Nms/rad
